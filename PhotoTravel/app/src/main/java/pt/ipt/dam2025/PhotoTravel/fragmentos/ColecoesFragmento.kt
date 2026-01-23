749c6b789c47ad0163f8acf570800c406c98148f
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2026-01-23 13:01:35 +0000

    deixar ui de iniciar sessao bonita

--- PhotoTravel/app/src/main/java/pt/ipt/dam2025/PhotoTravel/fragmentos/ColecoesFragmento.kt
+++ PhotoTravel/app/src/main/java/pt/ipt/dam2025/PhotoTravel/fragmentos/ColecoesFragmento.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import pt.ipt.dam2025.PhotoTravel.R
+import pt.ipt.dam2025.phototravel.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
